992957b3e8b4a69b90dbb4ab85fb29b7dfafc82a
Leela Mohan <lmohan@salesforce.com>
2021-06-17 22:26:31 +0530

    checkin changes

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var config = require('./config.js');
-var nforce = require('nforce');
 var jsforce = require('jsforce');
 var routes = require('./routes/index');
 
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-// get a reference to the socket once a client connects
 var socket = io.sockets.on('connection', function (socket) { });
 
 var replayId = -1; // -1 = Only New messages | -2 = All Window and New
@@ -42,44 +40,7 @@ var conn = new jsforce.Connection();
 	});
 });
 
-/*
-var org = nforce.createConnection({
-  clientId: config.CLIENT_ID,
-  clientSecret: config.CLIENT_SECRET,
-  redirectUri: config.CALLBACK_URL ,
-  mode: 'single',
-  environment: config.ENVIRONMENT  // optional, sandbox or production, production default
-});
-
-org.authenticate({ username: config.USERNAME, password: config.PASSWORD }, function(err, oauth) {
-
-  if(err) return console.log(err);
-  if(!err) {
-    console.log('*** Successfully connected to Salesforce ***');
-   
-  }
-
-  var cj = org.createStreamClient({ topic: '/data/CaseChangeEvent', replayId: -2 });
-	var str = cj.subscribe({ topic: '/data/CaseChangeEvent', oauth: oauth });
-
-	str.on('connect', function(){
-		console.log('Connected to topic: ' + '/data/CaseChangeEvent');
-	});
-
-	str.on('error', function(error) {
-		console.log('Error received from topic: ' + error);
-	});
-
-	str.on('data', function(data) {
-		console.log('Received the following from topic ---');
-		console.log(data);
-		socket.send(JSON.stringify(data));
-	});
-});*/
-
-
-
-// view engine setup
+// setup view engine 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
@@ -88,20 +49,18 @@ app.use(function(req, res, next){
   next();
 });
 
-// uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
-// catch 404 and forward to error handler
+// error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
-
-// development error handler
-// will print stacktrace
+ 
+// development error handler - print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -112,8 +71,7 @@ if (app.get('env') === 'development') {
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
+// production error handler - no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
--- config.js
+++ config.js
@@ -2,17 +2,6 @@ exports.PORT = process.env.PORT || 3001; // use heroku's dynamic port or 3001 if
 exports.DEBUG = true;
 exports.ENVIRONMENT = 'production';
 
-/*
-exports.CALLBACK_URL = process.env.CALLBACKURL ;
-exports.TOPIC = '/event/' + process.env.EVENTCHANNEL ;
-exports.REPLAY_ID = -1;
-
-exports.CLIENT_ID = process.env.CLIENTID;
-exports.CLIENT_SECRET = process.env.CLIENTSECRET;
-exports.USERNAME = process.env.USERNAME;
-exports.PASSWORD = process.env.PASSWORD;*/
-
-
 exports.USERNAME = process.env.USERNAME;
 exports.PASSWORD = process.env.PASSWORD;
 exports.SECURITYTOKEN = process.env.SECURITYTOKEN;
