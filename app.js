851608c4e0c520abcbaa8a11f9313d1c2472b76b
Leela Mohan <lmohan@salesforce.com>
2021-06-22 16:09:06 +0530

    code refactor

--- app.js
+++ app.js
@@ -14,9 +14,6 @@ var socket = io.sockets.on('connection', function (socket) { });
 var replayId = -1; // -1 = Only New messages | -2 = All Window and New
 
 var channel = '/data/CaseChangeEvent';
-/*var user = config.USERNAME;
-var pass = config.PASSWORD;
-var securityToken = config.SECURITYTOKEN ;*/
 const conn = new jsforce.Connection();
 
  getToken({
@@ -51,28 +48,6 @@ const conn = new jsforce.Connection();
   }
 
 });
-
-/*
- conn.login(user, pass + securityToken, function (err, res) {
-	console.log('loggedin');
-	if (err) {
-		return console.error(err);
-	}
-
-	var client = conn.streaming.createClient([
-		new jsforce.StreamingExtension.Replay(channel, replayId),
-		new jsforce.StreamingExtension.AuthFailure(function () {
-			console.log('failed');
-			return process.exit(1);
-		}),
-	]);
-
-	subscription = client.subscribe(channel, function (data) {
-		console.log('Received CDC Event');
-		socket.send(JSON.stringify(data));
-		console.log('Data sent to clients!!');
-	});
-}); */
 
 // setup view engine 
 app.set('views', path.join(__dirname, 'views'));
